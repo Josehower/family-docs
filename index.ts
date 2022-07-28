1439781afce2a06da8f9ee59499bf2de11eba41e
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2022-07-28 23:12:39 +0200

    Update host and port based on docs

--- index.ts
+++ index.ts
@@ -6,14 +6,20 @@ app.get('/', async (req) => {
   return await { family: `${req.protocol}://${req.hostname}/family-members` };
 });
 
-app.get('/family-members', async () => {
+app.get('/family-members', () => {
   return [{ person1: 'john' }];
 });
 
-app.listen({ port: 8080 }, (err, address) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`Server listening at ${address}`);
-});
+app.listen(
+  {
+    port: Number(process.env.PORT) || 8080,
+    host: process.env.HOST || '0.0.0.0',
+  },
+  (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+  },
+);
